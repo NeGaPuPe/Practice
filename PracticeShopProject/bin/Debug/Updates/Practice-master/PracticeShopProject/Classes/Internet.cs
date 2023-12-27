d79aaddd8599e48e4e3a3e6ce6a46ec96c014a8c
negapupe <rich416@mail.ru>
2023-12-27 21:57:52 +0300

    files

---
+++ PracticeShopProject/bin/Debug/Updates/Practice-master/PracticeShopProject/Classes/Internet.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeShopProject.Classes
+{
+    public class Internet
+    {
+        public static bool ok()
+        {
+            try
+            {
+                Dns.GetHostEntry("github.com");
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
