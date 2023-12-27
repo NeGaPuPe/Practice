aba676efb6f3cf547cbd5f2cec21046eb38f1ed7
negapupe <rich416@mail.ru>
2023-12-28 00:30:02 +0300

    files

--- PracticeShopProject/Pages/ProductsPage.xaml.cs
+++ PracticeShopProject/Pages/ProductsPage.xaml.cs
@@ -37,7 +37,6 @@ namespace PracticeShopProject.Pages
         }
         List<Product> productList = new List<Product>();
 
-        string CurrentVersionApp = Assembly.GetExecutingAssembly().GetName().Version.ToString();
         public void Sorting(object sender, EventArgs e)
         {
             var product = DB.entities.Product.ToList();
